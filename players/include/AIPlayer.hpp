9144b257f2199621ff1263483b9373a312766103
AlanHacker <alan.92925@gmail.com>
2025-06-12 00:30:23 +0800

    Manage AIPlayer strategy lifetime

--- players/include/AIPlayer.hpp
+++ players/include/AIPlayer.hpp
@@ -1,13 +1,14 @@
 // AIPlayer.hpp
 #pragma once
 
+#include <memory>
 #include "AIAlgorithm.hpp"
 #include "IPlayer.hpp"
 class AIPlayer : public IPlayer {
-    AIAlgorithm *strategy_;
+    std::unique_ptr<AIAlgorithm> strategy_;
 
 public:
-    explicit AIPlayer(AIAlgorithm *strategy);
+    explicit AIPlayer(std::unique_ptr<AIAlgorithm> strategy);
     AIPlayer();
     std::pair<int, int> getMove(const Board &board, int8_t player) override;
 };
--- players/src/AIPlayer.cpp
+++ players/src/AIPlayer.cpp
@@ -1,13 +1,12 @@
 #include "AIPlayer.hpp"
 #include "NegamaxStrategy.hpp"
 
-namespace {
-NegamaxStrategy defaultStrategy;
+AIPlayer::AIPlayer(std::unique_ptr<AIAlgorithm> strategy)
+    : strategy_(std::move(strategy)) {
+    if (!strategy_) strategy_ = std::make_unique<NegamaxStrategy>();
 }
 
-AIPlayer::AIPlayer(AIAlgorithm *strategy) : strategy_(strategy ? strategy : &defaultStrategy) {}
-
-AIPlayer::AIPlayer() : strategy_(&defaultStrategy) {}
+AIPlayer::AIPlayer() : strategy_(std::make_unique<NegamaxStrategy>()) {}
 
 std::pair<int, int> AIPlayer::getMove(const Board &board, int8_t player) {
     return strategy_->computeMove(board, player);
