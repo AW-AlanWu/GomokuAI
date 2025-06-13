1d4ce144ea552f659e1c423463b7ddfde754cfa5
AW-AlanWu <alan.92925@gmail.com>
2025-06-13 23:07:52 +0800

    patch: add file IO

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,4 +1,5 @@
 // Game.cpp
+#include <fstream>
 #include <iostream>
 
 #include "Game.hpp"
@@ -21,6 +22,8 @@ Game::~Game() {
 }
 
 void Game::play() {
+    int p1_cnt = 0, p2_cnt = 0;
+    std::ofstream out("GameResult.log", std::ios::app);
     while (true) {
         renderer_.draw();
         auto &player = (current_player_ == 1 ? p1_ : p2_);
@@ -29,16 +32,22 @@ void Game::play() {
             term_.moveCursor(Board::N + 6, 1);
             term_.clearLine();
             std::cout << "Draw!\n";
+            std::cout << "p1 use " << p1_cnt << " moves, p2 use " << p2_cnt << " moves!\n";
+            out << "0 " << p1_cnt << " " << p2_cnt << "\n";
             break;
         }
         board_.place(r, c, current_player_);
+        (current_player_ == 1 ? ++p1_cnt : ++p2_cnt);
         if (board_.checkWin(r, c)) {
             renderer_.draw();
             term_.moveCursor(Board::N + 6, 1);
             term_.clearLine();
             std::cout << "Player " << (current_player_ == 1 ? "●" : "○") << " wins!\n";
+            std::cout << "p1 use " << p1_cnt << " moves, p2 use " << p2_cnt << " moves!\n";
+            out << (int)current_player_ << " " << p1_cnt << " " << p2_cnt << "\n";
             break;
         }
         current_player_ = -current_player_;
     }
+    out.close();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 // main.cpp
 #include <cstring>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
 
 #include "AIPlayer.hpp"
@@ -13,6 +15,33 @@ int main(int argc, char *argv[]) {
     for (int i = 1; i < argc; ++i) {
         if (std::strcmp(argv[i], "--p1=ai") == 0) p1_ai = true;
         if (std::strcmp(argv[i], "--p2=ai") == 0) p2_ai = true;
+        if (std::strcmp(argv[i], "--history") == 0) {
+            std::ifstream ofs("GameResult.log");
+            if (!ofs.is_open()) {
+                std::cout << "尚無對局紀錄...\n";
+                return 0;
+            }
+            int game_cnt = 0;
+            int result, p1_cnt, p2_cnt;
+            while (ofs >> result >> p1_cnt >> p2_cnt) {
+                std::cout << "第 " << ++game_cnt << " 局: ";
+                if (result == 0) {
+                    std::cout << "Draw, ";
+                } else {
+                    std::cout << "player " << (result == 1 ? "1" : "2") << " win, ";
+                }
+                std::cout << "p1 use " << p1_cnt << " moves, p2 use " << p2_cnt << " moves!\n";
+            }
+            return 0;
+        }
+        if (std::strcmp(argv[i], "--clear") == 0) {
+            if (std::filesystem::remove("GameResult.log")) {
+                std::cout << "對局紀錄已清除\n";
+            } else {
+                std::cout << "尚無對局紀錄...\n";
+            }
+            return 0;
+        }
     }
 
     Terminal term;
